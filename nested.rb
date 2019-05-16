7261baa34232f94c8486e45452e342fa6c1e00a3
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 19:08:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 	programmer_hash[:yukihiro_matsumoto] = {
 	   :known_for => "Ruby",
      :languages => ["LISP", "C"]}
 
+	programmer_hash
 end
 
 def changing_alan
@@ -106,7 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
+	programmer_hash
 
 end
 
